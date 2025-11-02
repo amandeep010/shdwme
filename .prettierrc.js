8493eb4d58294f73f2679b5fc3b1d2a686574f74
aman <7618deep@gmail.com>
2025-11-02 10:50:44 +0000

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	trailingComma: "none",
+	tabWidth: 2,
+	printWidth: 80,
+	bracketSpacing: false,
+	useTabs: true,
+	semi: false,
+	singleQuote: false,
+	quoteProps: "consistent",
+	bracketSameLine: false,
+	arrowParens: "always",
+	proseWrap: "always",
+	endOfLine: "lf",
+	embeddedLanguageFormatting: "auto",
+	singleAttributePerLine: false,
+	parser: "typescript" // Explicitly add this for TypeScript files
+}
